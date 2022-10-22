8c36a7a547f45f5a9662c818916728c5269a503b
Divided by Zer0 <mail@dbzer0.com>
2022-10-23 01:08:14 +0200

    fixed image exceediing viewport

--- ImagePreview.gd
+++ ImagePreview.gd
@@ -4,7 +4,11 @@ var source_image: Image
 
 func load_image_from_path(path: String) -> void:
 	source_image = Image.new()
-	source_image.load(path)
+	var error = source_image.load(path)
+	if error != OK:
+		var error_msg := "Couldn't load the image."
+		push_error(error_msg)
+		return
 	var image_texture = ImageTexture.new();
 	image_texture.create_from_image(source_image)
 	texture = image_texture
