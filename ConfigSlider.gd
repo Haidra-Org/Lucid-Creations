29078545280caab03543868aba1067af94684299
Divided by Zer0 <mail@dbzer0.com>
2022-11-12 23:38:49 +0100

    larger values setting

--- ConfigSlider.gd
+++ ConfigSlider.gd
@@ -14,19 +14,22 @@ const CONFIG := {
 	"width": {
 		"label": "Width",
 		"min": 64,
-		"max": 1024,
+		"max": 3072,
+		"upfront_limit": 640,
 		"step": 64,
 	},
 	"height": {
 		"label": "Height",
 		"min": 64,
-		"max": 1024,
+		"max": 3072,
+		"upfront_limit": 640,
 		"step": 64,
 	},
 	"steps": {
 		"label": "Sampling Steps",
 		"min": 1,
-		"max": 100,
+		"max": 500,
+		"upfront_limit": 50,
 		"step": 1,
 	},
 	"cfg_scale": {
@@ -42,6 +45,7 @@ const CONFIG := {
 		"step": 0.01,
 	},
 }
+var upfront_limit = null
 onready var h_slider = $"%HSlider"
 onready var config_name = $"%ConfigName"
 onready var config_value = $"%ConfigValue"
@@ -50,6 +54,7 @@ export(String, "amount", "width", "height","steps", "cfg_scale", "denoising_stre
 
 func _ready():
 	_adapt_to_config_name()
+	globals.connect("setting_changed", self, "_on_setting_changed")
 
 func set_value(value) -> void:
 	$"%HSlider".value = value
@@ -67,12 +72,30 @@ func set_config_name(value) -> void:
 func _adapt_to_config_name() -> void:
 	if Engine.editor_hint and get_child_count() == 0:
 		return
-	# Can't use the onready names as they're not set in the editor
+	# WARNING: Can't use the onready names as they're not set in the editor
 	$"%ConfigName".text = CONFIG[config_setting].label
 	$"%HSlider".min_value = CONFIG[config_setting].min
 	$"%HSlider".max_value = CONFIG[config_setting].max
 	$"%HSlider".step = CONFIG[config_setting].step
+	if CONFIG[config_setting].has('upfront_limit'):
+		upfront_limit = CONFIG[config_setting].upfront_limit
+		if not globals.config.get_value("Options", "larger_values", false):
+			$"%HSlider".max_value = CONFIG[config_setting].upfront_limit
 
 
 func _on_HSlider_value_changed(value):
 	config_value.text = str(value)
+	if upfront_limit != null and upfront_limit < value:
+		config_value.modulate = Color(1,0,0)
+		h_slider.modulate = Color(1,0,0)
+	else:
+		config_value.modulate = Color(1,1,1)
+		h_slider.modulate = Color(1,1,1)
+
+func _on_setting_changed(setting_name):
+	if setting_name == "larger_values" and CONFIG[config_setting].has('upfront_limit'):
+		if globals.config.get_value("Options", "larger_values", false):
+			h_slider.max_value = CONFIG[config_setting].max
+		else:
+			h_slider.max_value = CONFIG[config_setting].upfront_limit
+			
--- globals.gd
+++ globals.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal setting_changed(setting_name)
+
 const CONFIG_FILENAME = "user://settings.cfg"
 var config = ConfigFile.new()
 
@@ -13,6 +15,7 @@ func _ready() -> void:
 func set_setting(config_name: String, value, section := "Parameters") -> void:
 	config.set_value(section, config_name, value)
 	config.save(CONFIG_FILENAME)
+	emit_signal("setting_changed",config_name)
 
 # Initiates game_settings from the contents of CFConst.SETTINGS_FILENAME
 func init_config_from_file() -> void:
