7ffa5c8fc773bd4f3d79de8e3aa61991df3f6d8d
db0 <mail@dbzer0.com>
2023-06-07 18:55:35 +0200

    allow selecting files via images

--- src/Buttons/LoadFromDisk.gd
+++ src/Buttons/LoadFromDisk.gd
@@ -11,9 +11,13 @@ func _on_LoadFromDiskNativeDialogOpenFile_files_selected(files: Array):
 	if files.size() == 0:
 		return
 	var filename: String = files[0]
+	var file = File.new()
+	if filename.ends_with(".png"):
+		filename = filename.get_basename() + '.json'
+		if not file.file_exists(filename):
+			return
 	globals.set_setting("load_initial_path", filename.get_base_dir() + '/', "Options")
 	load_from_disk_native_dialog_open_file.initial_path = filename.get_base_dir() + '/'
-	var file = File.new()
 	file.open(filename, File.READ)
 	var data = JSON.parse(file.get_as_text())
 	file.close()
