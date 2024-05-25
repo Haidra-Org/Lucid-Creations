fb64dac10af01839c32f55ecf5aee3adec61e145
db0 <mail@dbzer0.com>
2024-05-26 00:04:19 +0200

    feat: allow to switch to godot native browse

--- PopupInfoPanel.gd
+++ PopupInfoPanel.gd
@@ -43,6 +43,7 @@ const DESCRIPTIONS = {
 	"WorkerAutoComplete": "Specify workers to use for this generation. Use the toggle below to specify using them as an allowlist or a blocklist. When models are selected, only workers which can generate any of those models will be shown.",
 	"ShowAllWorkers": "Press this button to display and select available workers for your selected model.",
 	"AllowDowngrade": "When enabled and your account doesn't have enough kudos to generate the image, it will be downgraded in resolution and steps, so that it does not need upfront kudos, instead of failing.",
+	"UseGodotBrowse": "When enabled instead of using your OS' native browse dialog, it will use Godot Engine's built-in node.",
 	# Control Buttons
 	"Prompt": "Prompt: This tab allows you to specify the prompt which will generate the image",
 	"img2img": "Img2Img: This tab allows you to specify image-to-image functionality, such as the source image or controlnets.",
